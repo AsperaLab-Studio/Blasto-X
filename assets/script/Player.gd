4a154902508aeef885b4b8de4e83aeeb5df0d0b5
gabriel <gabriel@gabriel.gabriel>
2021-06-27 23:18:00 +0200

    add move animation

--- assets/script/Player.gd
+++ assets/script/Player.gd
@@ -14,18 +14,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = _get_direction()
-	move_and_slide(direction*speed)
+	
+	if (Input.is_action_pressed("move_right")) or (Input.is_action_pressed("move_left")) or (Input.is_action_pressed("move_up")) or (Input.is_action_pressed("move_down")):
+		move_and_slide(direction*speed)
+	else:
+		sprite.play("idle")
+	
 
 func _get_direction() -> Vector2:
 	var direction: = Vector2()
 	
 	if Input.is_action_pressed("move_right"):
+		sprite.play("move")
 		direction.x = 1
 	if Input.is_action_pressed("move_left"):
+		sprite.play("move")
 		direction.x = -1
 	if Input.is_action_pressed("move_up"):
+		sprite.play("move")
 		direction.y = -1
 	if Input.is_action_pressed("move_down"):
+		sprite.play("move")
 		direction.y = 1
 	
 	return direction
