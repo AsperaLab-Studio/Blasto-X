72d53a218c1a242ad912d1f413cdcf69e64680de
gabriel <gabriel@gabriel.gabriel>
2021-07-19 21:32:17 +0200

    light modify to scripts structure

--- assets/Scripts/Player.gd
+++ assets/Scripts/Player.gd
@@ -5,52 +5,60 @@ onready var sprite: AnimatedSprite = $AnimatedSprite
 onready var attack_collision: Area2D = $Pivot/AttackCollision 
 onready var pivot: Node2D = $Pivot
 
+enum STATE {IDLE, MOVE, ATTACK, SHOOT, WIN, LOSE}
+
 export(int) var speed: int = 300
 export(bool) var moving: bool = false
 export(Vector2) var direction: = Vector2.ZERO
-export(String) var state = "IDLE"
+export(STATE) var current_state = STATE.IDLE
 
 func _ready() -> void:
 	sprite.play("idle")
 	attack_collision.monitoring = false
 
 func _process(delta: float) -> void:
-	if state == "IDLE" && Input.is_action_just_pressed("attack"):
-		state = "ATTACK"
-		return
-		
-	if state == "ATTACK":
-		attack_collision.monitoring = true
-		sprite.play("attack")
-		sprite.connect("animation_finished", self, "_on_AnimatedSprite_animation_finished")
-		return
-		
+	
 	direction = _get_direction()
 	
-	if direction:
-		if direction.x < 0:
-			sprite.flip_h = true
-			if pivot.scale.x > 0:
-				pivot.scale.x = - pivot.scale.x
-		elif direction.x > 0:
-			sprite.flip_h = false
-			if pivot.scale.x < 0:
-				pivot.scale.x = - pivot.scale.x
-		move_and_slide(direction * speed)
-		sprite.play("move")
-	else:
-		sprite.play("idle")
+	match current_state:
+		STATE.IDLE:
+			if Input.is_action_just_pressed("attack"):
+				current_state = STATE.ATTACK
+				
+			if direction:
+				current_state = STATE.MOVE
+			else:
+				sprite.play("idle")
+				
+		STATE.ATTACK:
+			attack_collision.monitoring = true
+			sprite.play("attack")
+			sprite.connect("animation_finished", self, "_on_AnimatedSprite_animation_finished")
+			
+		STATE.MOVE:
+			if direction.x < 0:
+				sprite.flip_h = true
+				if pivot.scale.x > 0:
+					pivot.scale.x = - pivot.scale.x
+			elif direction.x > 0:
+				sprite.flip_h = false
+				if pivot.scale.x < 0:
+					pivot.scale.x = - pivot.scale.x
+			move_and_slide(direction * speed)
+			sprite.play("move")
+		
 	
 
 func _on_AnimatedSprite_animation_finished():
-	state = "IDLE"
+	current_state = STATE.IDLE
 	attack_collision.monitoring = false
 	
-	
+
 func _on_AttackCollision_area_entered(area: Area2D) -> void:
 	if area.owner.is_in_group("enemy"):
 		var enemy = area.owner as Enemy
 		enemy.hit()
+	
 
 func _get_direction() -> Vector2:
 	var input_direction = Vector2()
