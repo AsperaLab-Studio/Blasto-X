8c8e431f07c771a71ca199dfdd3cac2b99e36846
Alien089 <gabri.moratelli@gmail.com>
2023-10-24 20:21:18 +0200

    working on rework of floor generation

--- assets/Scripts/FloorCreator.gd
+++ assets/Scripts/FloorCreator.gd
@@ -3,7 +3,9 @@ extends Node
 
 export var increment = 1566
 export var y = 768
+export var x = 768
 export var n_positions = 5
+export(bool) var horizonal_vertical = false
 export(Array, PackedScene) var scenes
 
 func _ready() -> void:
@@ -11,5 +13,8 @@ func _ready() -> void:
 	for n in n_positions:
 		var rand_index:int = randi() % scenes.size()
 		var scene_instance = scenes[rand_index].instance()
-		scene_instance.position = Vector2(increment * (n + 1), y)
+		if !horizonal_vertical:
+			scene_instance.position = Vector2(increment * (n + 1), y)
+		else:
+			scene_instance.position = Vector2(x, increment * (n + 1))
 		add_child(scene_instance)
